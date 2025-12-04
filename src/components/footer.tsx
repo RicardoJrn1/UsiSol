96d97577c8e3b8814864264bd48053d37be22483
Ricardo Alves <ricardo727jrn@gmail.com>
2025-12-04 16:14:39 -0300

    Correção da cidade

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -102,7 +102,7 @@ export default function Footer() {
             <div>
               <h3 className="text-sm font-semibold text-white">Contato</h3>
               <ul className="mt-5 space-y-3 text-sm text-gray-400">
-                <li>Campinas - SP</li>
+                <li>Dois Viznhos - PR</li>
                 <li>
                   <a
                     href={WHATSAPP_LINK}
